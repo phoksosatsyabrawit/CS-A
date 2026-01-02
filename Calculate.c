fb8eb3050ca6bdda1f70707cf6a39c18fc8c8d70
phoksosatsyabrawit <phoksosatsyabrawit@gmail.com>
2026-01-03 00:08:59 +0700

    operator

--- Calculate.c
+++ Calculate.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void)
+{
+    char s [5];
+    int x,y;
+    printf("x: ");
+    fgets(s, sizeof(s), stdin);
+    x = atoi(s);
+    printf("y: ");
+    fgets(s, sizeof(s), stdin);
+    y = atoi(s);
+    printf("%i\n", x+y);
+}
