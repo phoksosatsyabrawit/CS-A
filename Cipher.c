dce788f52e1c38dc2f6d5afd7b0a0d22a9c5d9d7
phoksosatsyabrawit <phoksosatsyabrawit@gmail.com>
2026-01-03 21:56:57 +0700

    cipher

--- Cipher.c
+++ Cipher.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_int (const char *prompt);
+
 int main (void)
 {
-    printf("Hello, world \n");
+    int anw = get_int("What's your name: ");
+    printf("%s\n", anw);
+}
+
+int get_int (const char *prompt)
+{
+    char p [32];
+    printf("%s", prompt);
+    fgets(p, sizeof(p), stdin);
+    return atoi(p);
 }
