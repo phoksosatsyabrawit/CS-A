ba501076d6e8f6363d49c2f6019eb63a47ebc2a8
phoksosatsyabrawit <phoksosatsyabrawit@gmail.com>
2026-01-03 12:10:39 +0700

    format codes

--- Calculate.c
+++ Calculate.c
@@ -3,13 +3,13 @@
 
 int main(void)
 {
-    char s [5];
-    int x,y;
+    char s [13];
+    long x,y;
     printf("x: ");
     fgets(s, sizeof(s), stdin);
     x = atoi(s);
     printf("y: ");
     fgets(s, sizeof(s), stdin);
     y = atoi(s);
-    printf("%i \n", x+y);
+    printf("%li \n", x + y);
 }
