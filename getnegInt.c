78e9308a87262e26c91efec745ae8bab215a220f
phoksosatsyabrawit <phoksosatsyabrawit@gmail.com>
2026-01-07 20:00:18 +0700

    re-printing prompt if the value is less then zero

--- getnegInt.c
+++ getnegInt.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_negInt (char *prompt);
+
+int main (void)
+{
+    int neg = get_negInt("Negative Interger: ");
+    printf("%i \n", neg);
+}
+
+int get_negInt (char *prompt)
+{
+    int x;
+    char n [32];
+
+    do
+    { 
+       printf("%s", prompt);
+       fgets(n, sizeof(n), stdin);
+       x = atoi(n);
+    }
+    while (x < 0);
+    
+    return x;
+}
 
